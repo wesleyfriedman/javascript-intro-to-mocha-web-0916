36674cbe24677016d3600a98a1ab10849acbfeba
wesleyfriedman <friedman.wesley@gmail.com>
2016-11-05 18:05:01 -0400

    Done

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth(){
+  return true
+}
+
+function sayHey(){
+  return 'Hey!'
+}
+
+function sayHeyFriend(name){
+  return `Hey, ${name}!`
+}
+
+function favoriteIceCream(name){
+  return `I love ${name}`
+}
+
+function shouting(string){
+  return string.toUpperCase()
+}
+
+function roundDown(num){
+  return Math.floor(num)
+}
